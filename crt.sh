e6e6009c72fe7083151db8724ef7ec3e0a186d41
az7rb <54269260+az7rb@users.noreply.github.com>
2022-04-23 21:54:27 +0300

    Update ctt.sh v 1.1

--- crt.sh
+++ crt.sh
@@ -2,34 +2,73 @@
 
 echo "
 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|      ..| search crt.sh v1.0 |..     |
+|      ..| search crt.sh v1.1 |..     |
 +   site : crt.sh Certificate Search  +
 |            Twitter: az7rb           |
 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 	"
-	 
-	search() {
-	requestsearch="$(curl -s "https://crt.sh?q=%.$domain&output=json")"
+	
+	Help()
+{
+   # Display Help
+   echo "options:"
+   echo ""
+   echo "-h     Help"
+   echo "-d     Search Domain Name       | Example: $0 -d hackerone.com"
+   echo "-o     Search Organization Name | Example: $0 -o hackerone+inc"
+   echo ""
+}
+
+	# Request the Search  with Domain Name
+	Domain() {
+	requestsearch="$(curl -s "https://crt.sh?q=%.$req&output=json")"
 		 
-			 echo $requestsearch > search.txt
-			 cat search.txt | jq ".[].common_name,.[].name_value"| cut -d'"' -f2 | sed 's/\\n/\n/g' | sed 's/\*.//g'| sed -r 's/([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})//g' |sort | uniq > output/$domain.txt
-			 rm search.txt
+			 echo $requestsearch > req.txt
+			 cat req.txt | jq ".[].common_name,.[].name_value"| cut -d'"' -f2 | sed 's/\\n/\n/g' | sed 's/\*.//g'| sed -r 's/([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})//g' |sort | uniq > output/domain.$req.txt
+			 rm req.txt
 			 echo ""
-			 cat output/$domain.txt
+			 cat output/domain.$req.txt
 			 echo ""
-			 echo -e "\e[32m[+]\e[0m Total Save will be \e[31m"$(cat output/$domain.txt | wc -l)"\e[0m Domain only"
-			 echo -e "\e[32m[+]\e[0m Output saved in output/$domain.txt"
-}	 
+			 echo -e "\e[32m[+]\e[0m Total Save will be \e[31m"$(cat output/domain.$req.txt | wc -l)"\e[0m Domain only"
+			 echo -e "\e[32m[+]\e[0m Output saved in output/domain.$req.txt"
+}		 
+    # Request the Search with Organization Name
+	Organization() {
+	requestsearch="$(curl -s "https://crt.sh?q=$req&output=json")"
+		 
+			 echo $requestsearch > req.txt
+			 cat req.txt | jq ".[].common_name"| cut -d'"' -f2 | sed 's/\\n/\n/g' | sed 's/\*.//g'| sed -r 's/([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})//g' |sort | uniq > output/org.$req.txt
+			 rm req.txt
+			 echo ""
+			 cat output/org.$req.txt
+			 echo ""
+			 echo -e "\e[32m[+]\e[0m Total Save will be \e[31m"$(cat output/org.$req.txt | wc -l)"\e[0m Domain only"
+			 echo -e "\e[32m[+]\e[0m Output saved in output/org.$req.txt"
+}	
 
 if [ -z $1 ]
         then
-                echo "USAGE: $0 [domain.com] "
+                Help
                 exit
         else
-                domain=$1
+                req=$2
+                
 fi
 
-if [ -z $2 ]
-then
-search
-fi
+while getopts "h|d|o|" option; do
+   case $option in
+      h) # display Help
+         Help
+         ;;
+      d) # Search Domain Name
+	 Domain
+         ;;
+      o) # Search Organization Name
+	 Organization
+         ;;
+     *) # Invalid option
+          Help
+          ;;
+		 
+   esac
+done
